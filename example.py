1e05b484eedb8b54df6e860a4ce78958208470b7
Alexey <yakor.alex@gmail.com>
2023-07-17 09:38:36 +0200

    update example.py

--- example.py
+++ example.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Welcome to Flask!'
+
+
+@app.get('/users')
+def users_get():
+    return 'GET /users'
+
+
+@app.post('/users')
+def users_post():
+    return 'POST /users'
